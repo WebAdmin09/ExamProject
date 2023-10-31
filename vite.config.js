2102306efd1ec7a4ea4640487734cea7b5278f6a
WebAdmin09 <husanorifjonov9@gmail.com>
2023-10-31 05:56:13 +0500

    last commit

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,30 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-// import "./src/assets/libs/simplebar/dist/simplebar"
 
 // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+// vite.config.js
+
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id.toString().split('node_modules/')[1].split('/')[0].toString()
+          }
+        }
+      }
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      css: {
+        additionalData: `@import "./src/assets/libs/simplebar/dist/simplebar.css"`,
+      },
+    },
+  },
 })
