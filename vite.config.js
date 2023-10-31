ddb902f787b8b58f0e58dd750c7068d1d977ccf4
WebAdmin09 <husanorifjonov9@gmail.com>
2023-10-31 05:27:00 +0500

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import "simplebar"
+// import "./src/assets/libs/simplebar/dist/simplebar"
 
 // https://vitejs.dev/config/
 export default defineConfig({
